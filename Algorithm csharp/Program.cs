c44189b9f9f308b5aaa316994595861301ee070d
mcaleg2006 <emil720apro@gmail.com>
2025-11-05 14:56:47 +0200

    BinarySearch

--- Algorithm csharp/Program.cs
+++ Algorithm csharp/Program.cs
@@ -1,6 +1,32 @@
 ﻿
-class Algortihm
+class Algorithm
 {
+    public int BinarySearch(int[] array, int item)
+    {
+        int min = 0;
+        int max = array.Length - 1;
+
+        while (min <= max)
+        {
+            int mid = (min + max) / 2;
+            int guess = array[mid];
+
+            if (guess == item)
+            {
+                return mid;
+            }
+            else if (guess > item)
+            {
+                max = mid - 1;
+            }
+            else
+            {
+                min = mid + 1;
+            }
+        }
+
+        return min;
+    }
     
     
 }
@@ -9,6 +35,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        int[] arr = [1, 2, 3, 4, 5];
+        Algorithm algo = new Algorithm();
+        Console.WriteLine(algo.BinarySearch(arr, 5));
     }
 }
