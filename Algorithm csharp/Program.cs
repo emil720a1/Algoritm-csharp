0bd8dbfee4edf306b3d636ae4611d788e34b3a98
mcaleg2006 <emil720apro@gmail.com>
2025-11-06 10:03:12 +0200

    Recursion

--- Algorithm csharp/Program.cs
+++ Algorithm csharp/Program.cs
@@ -1,14 +1,36 @@
 ﻿
+using System.Reflection.PortableExecutable;
+
 class Algortihm
 {
-    
-    
+    public void RecursionSimple(int i)
+    {
+        Console.WriteLine(i);
+        if (i <= 1) return;
+        RecursionSimple(i - 1);
+    }
+
+
+    public int Factorial(int x)
+    {
+
+        if (x == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return x * Factorial(x - 1);
+        }
+    }
 }
 
 class Program
 {
     static void Main(string[] args)
     {
-        
+        Algortihm a = new Algortihm();
+        a.RecursionSimple(10);
+        Console.WriteLine(a.Factorial(10));
     }
 }
