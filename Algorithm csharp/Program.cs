14214691482d0bcfbac395734975ed80e1b38538
mcaleg2006 <emil720apro@gmail.com>
2025-11-07 19:50:46 +0200

    quicksort

--- Algorithm csharp/Program.cs
+++ Algorithm csharp/Program.cs
@@ -1,7 +1,31 @@
 ﻿
 class Algortihm
 {
-    
+    public static List<T> QuickSort<T>(List<T> list)
+    where T : IComparable<T>
+    {
+        if (list.Count < 2)
+        {
+            return list;
+        }
+        T pivot = list[0];
+        
+        
+        List<T> less = list
+            .Skip(1)
+            .Where(item => item.CompareTo(pivot) <= 0)
+            .ToList();
+
+        List<T> greater = list
+            .Skip(1)
+            .Where(item => item.CompareTo(pivot) > 0)
+            .ToList();
+        
+        return QuickSort<T>(less)
+            .Concat(new List<T> { pivot })
+            .Concat(QuickSort(greater))
+            .ToList();
+    }
     
 }
 
@@ -9,6 +33,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<int> list = new List<int> {10, 80 , 30, 90, 40, 50 , 70};
+        Console.WriteLine("Список до сортировки: " + string.Join(", ", list) + "");
         
+        List<int> sorted = Algortihm.QuickSort(list);
+        Console.WriteLine("Отсортированный список: " + string.Join(", ", sorted));
     }
 }
