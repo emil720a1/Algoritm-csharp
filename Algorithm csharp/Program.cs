783e47923ff166058929bf25ac460c367fbac77c
mcaleg2006 <emil720apro@gmail.com>
2025-11-07 18:34:19 +0200

    Gcd

--- Algorithm csharp/Program.cs
+++ Algorithm csharp/Program.cs
@@ -1,7 +1,33 @@
 ﻿
 class Algortihm
 {
-    
+    public static int GCD_Iteration(int a, int b)
+    {
+        a = Math.Abs(a);
+        b = Math.Abs(b);
+
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        
+        return a;
+    }
+
+
+    public static int GCD_Recursion(int a, int b)
+    {
+        if (b == 0)
+        {
+            return a;
+        }
+        else
+        {
+            return GCD_Recursion(b, a % b);
+        }
+    }
     
 }
 
